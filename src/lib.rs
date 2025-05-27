a7baa66f4d33899992f8b9c6533194484a26a6de
ash taylor?! <estrobiologist@outlook.com>
2025-05-27 14:33:17 +0200

    i dunno man

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,7 @@ use log::{debug, error};
 use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 
 #[cfg(target_os = "windows")]
-use windows::Win32::{Foundation::HWND, UI::WindowsAndMessaging::{SetParent, GetWindowRect, SetWindowPos, SWP_NOMOVE}};
+use windows::Win32::{Foundation::HWND, UI::WindowsAndMessaging::{SetParent, GetWindowRect, SetWindowPos, SWP_NOMOVE, SWP_NOSIZE}};
 
 /// Current FL SDK version.
 pub const CURRENT_SDK_VERSION: u32 = 1;
@@ -308,7 +308,6 @@ impl EditorHandle {
 
     #[cfg(target_os = "windows")]
     unsafe fn attach_editor_win<V: HasRawWindowHandle>(&self, view: &mut V) {
-
         if let RawWindowHandle::Win32(handle) = view.raw_window_handle() {
             if handle.hwnd.is_null() {
                 return;
@@ -321,6 +320,8 @@ impl EditorHandle {
             GetWindowRect(child, &mut rect).unwrap();
             SetWindowPos(parent, None, 0, 0, rect.right - rect.left, rect.bottom - rect.top, SWP_NOMOVE).unwrap();
             SetParent(child, Some(parent)).unwrap();
+            GetWindowRect(parent, &mut rect).unwrap();
+            SetWindowPos(child, None, rect.left, rect.top, 0, 0, SWP_NOSIZE).unwrap();
         }
     }
 }
